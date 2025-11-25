077e34d9731933079f7d4f678686a54fcad7f468
mahesh20071011-ui <mahesh20071011@gmail.com>
2025-11-25 14:21:10 +0530

    Add files via upload

---
+++ calcu.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	char ch;
+	printf("enter a&b values");
+	scanf("%d%d",&a,&b);
+	printf("your choice + add - sub * mult / div");
+	scanf(" %c",&ch);
+	switch(ch)
+	{
+		case '+':c=a+b;
+		printf("add=%d",c);
+		break;
+		case '-':c=a-b;
+		printf("sub=%d",c);
+		break;
+		case '*':c=a*b;
+		printf("mult=%d",c);
+		break;
+		case '/':c=a/b;
+		printf("div=%d",c);
+		break;
+		default:printf(" invalid choice");
+	}
+	return 0;
+}
