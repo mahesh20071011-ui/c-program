edf65142ea3a0076da86d41141fe9073c389c52a
mahesh20071011-ui <mahesh20071011@gmail.com>
2025-11-25 14:47:44 +0530

    Add files via upload

---
+++ 5 sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,   
+	printf"enter five subjects marks");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    printf("total=%d",tot);
+    printf("avg=%f",avg);
+    return 0;
+} 
+
+
+
