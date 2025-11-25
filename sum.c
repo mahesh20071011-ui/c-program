d16f6a9bb6ba39247c49e9648da3260ead96f721
mahesh20071011-ui <mahesh20071011@gmail.com>
2025-11-25 14:43:19 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+    float 
+	printf("enter a&b&c values ::");
+	scanf("%d%d%d",&a,&b,&c);
+	d=(a+b+c)/3;
+	printf("average of the three numbers %d ", d);
+	return 0;
+}
